f497ba41bca2f7c0aa46be0361935a0158349dd0
vasiliyvn <mishadasha1114@gmail.com>
2024-12-25 12:28:45 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+# 1st program
+print(9 ** 0.5 * 5)
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+#3rd program
+print(a := 2*2+2)
+print(b := 2*(2+2))
+#3rd program
+print(a == b)
+# 4th program
+print(int(float('123.456')*10)%10)
